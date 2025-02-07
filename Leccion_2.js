aae3ff968536cfd9d432955f4222eef993704a41
Evelyn Hernandez <eve@Evelyns-MacBook-Air.local>
2025-02-06 18:14:19 -0600

    Lección 2 - Estructuras de control if

--- Leccion_2.js
+++ Leccion_2.js
@@ -1,6 +1,9 @@
-const nota = 45;
+const nota = -1;
 
-if(nota>=90){
+if(nota>100 || nota<0){
+    console.log("Ingrese un número que este en los rangos de 0 a 100");
+}
+else if(nota>=90){
     console.log("Excelente");
 }
 else if(nota>=75 && nota<=89){
