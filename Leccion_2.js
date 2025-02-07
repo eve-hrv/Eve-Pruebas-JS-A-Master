2989f75d1f34ee5b517c1ed5d906e869e3697a40
Evelyn Hernandez <eve@Evelyns-MacBook-Air.local>
2025-02-06 18:06:39 -0600

    Leccion 2 - Uso de condiciones

---
+++ Leccion_2.js
@@ -0,0 +1,14 @@
+const nota = 45;
+
+if(nota>=90){
+    console.log("Excelente");
+}
+else if(nota>=75 && nota<=89){
+    console.log("Bien");
+}
+else if(nota>=60 && nota<=74){
+    console.log("Suficiente");
+}
+else{
+    console.log("No aprueba");
+}
