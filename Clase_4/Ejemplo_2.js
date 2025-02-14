952406980c2c2c8b7ecd7c3b8f82b0248fdb7621
Evelyn Hernandez <eve@Evelyns-MacBook-Air.local>
2025-02-13 20:25:28 -0600

    Ejemplo 2 - Clase 4

---
+++ Clase_4/Ejemplo_2.js
@@ -0,0 +1,12 @@
+let celcius;
+let farenheit;
+
+
+let cAf = (c) => ((c * 9/5) + 32)
+let fAc = (f) => ((f - 32) * 5/9);
+
+celcius = 10
+farenheit = 50
+
+console.log(cAf(celcius))
+console.log(fAc(farenheit))
